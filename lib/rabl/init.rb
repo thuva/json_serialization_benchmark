96ea85dcd6f692ef2c2a8cd4d40210cdf79c3cd3
Thuva Tharma <tthuvarakan@gmail.com>
2014-05-27 15:25:18 -0400

    Enable template lookup caching for RABL

--- lib/rabl/init.rb
+++ lib/rabl/init.rb
@@ -1,6 +1,6 @@
 Rabl.configure do |config|
   config.json_engine = JsonEncoder
-  config.cache_sources = false
+  config.cache_sources = true
   config.include_json_root = false
   config.enable_json_callbacks = true
 end
